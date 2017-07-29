6db2c52550acffc995f1562e317e45b9c008c436
Charles P. Oliver <charles.oliver.extern@holidaycheck.com>
2017-07-29 21:44:07 +0200

    3.16 - Exercises: Address Book Functions

--- src/Data/AddressBook.purs
+++ src/Data/AddressBook.purs
@@ -3,8 +3,7 @@ module Data.AddressBook where
 import Prelude
 
 import Control.Plus (empty)
-import Data.BooleanAlgebra (class BooleanAlgebra)
-import Data.List (List(..), filter, head)
+import Data.List (List(..), filter, head, nubBy, null)
 import Data.Maybe (Maybe)
 import Data.String (joinWith)
 
@@ -25,6 +24,33 @@ type AddressBook = List Entry
 emptyBook :: AddressBook
 emptyBook = empty
 
+containsName :: String -> String -> AddressBook -> Boolean
+containsName firstName lastName book = not $ null $ filter filterEntry book
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+findEntryByAddress :: String -> AddressBook -> Maybe Entry
+findEntryByAddress street book = (filter filterEntry >>> head) book
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.address.street == street
+
+findEntryByName :: String -> String -> AddressBook -> Maybe Entry
+findEntryByName firstName lastName book = (filter filterEntry >>> head) book
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+insertEntry :: Entry -> AddressBook -> AddressBook
+insertEntry = Cons
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates book = nubBy isDuplicate book
+  where
+    isDuplicate :: Entry -> Entry -> Boolean
+    isDuplicate a b = a.firstName == b.firstName && a.lastName == b.lastName
+
 showAddress :: Address -> String
 showAddress addr = joinWith ", " [addr.street, addr.city, addr.state]
 
@@ -32,12 +58,3 @@ showEntry :: Entry -> String
 showEntry entry = entry.lastName <> ", " <>
                   entry.firstName <> ": " <>
                   showAddress entry.address
-
-findEntry :: String -> String -> AddressBook -> Maybe Entry
-findEntry firstName lastName book = (filter filterEntry >>> head) book
-  where
-    filterEntry :: Entry -> Boolean
-    filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
-
-insertEntry :: Entry -> AddressBook -> AddressBook
-insertEntry = Cons
